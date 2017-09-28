0db11456067eeff0cd4732b06834f2ba7850e230
Marcin Kałużny <marcin.kaluzny@me.com>
2017-09-28 18:46:20 +0200

    zadanie domowe 2

---
+++ exercises/[27092017]/task2.js
@@ -0,0 +1,12 @@
+/**
+ * Zadanie 2.
+ *
+ * Zapoznaj się z koncepcją programowania funkcyjnego - compose
+ *
+ * Napisz taką funkcję lub znajdz w internecie i przerób skrypty (funkcje) z zadania 1
+ * tak zeby mozna bylo uzyć compose i uzyskac za jej pomocą rozwiązanie zadania 1.
+ */
+
+function compose() {
+
+}
